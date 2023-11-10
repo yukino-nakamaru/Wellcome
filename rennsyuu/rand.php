80baa3ac8e3e7f4f0f7e9c36946eb14430dc9b0c
yukino-nakamaru <y.nakamaru23@gmail.com>
2023-11-10 19:37:54 +0900

    comit!

---
+++ rennsyuu/rand.php
@@ -0,0 +1,17 @@
+<?php
+
+echo rand(0,100);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
